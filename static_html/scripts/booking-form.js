879b13d5fdaae4863d953ac8f65da4c9ef3c7645
johnlivingprooff <johnlivingprooff@gmail.com>
2024-02-29 16:03:24 +0200

    Re-doing homepage booking form

--- static_html/scripts/booking-form.js
+++ static_html/scripts/booking-form.js
@@ -1,62 +1,10 @@
 const vm = Vue.createApp({
-    data() {
-        return{
-            reservation: {
-            checkin: '',
-            checkout: '',
-            guests: '1'
-            }
-        };
-    },
+    template: `
+    
+    `,
     methods: {
         submitReservation() {
-            this.sendDataToServer();
-
-            this.reservation = {
-                checkin: '',
-                checkout: '',
-                guests: '1'
-            };
-        },
-        sendDataToServer() {
-            const baseUri = process.env.VUE_APP_BASE_URL;
-            console.log('Sending reservation data to server:', this.reservation);
-            // implementing API call here
-            fetch(`${baseUri}/b/reservations/`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(this.reservation)
-            })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+            console.log('Reservation submitted:', this.reservation)
         }
     },
-    template: `
-    <h2>Book Your Stay</h2>
-    <p>Required fields are followed by *</p>
-
-    <form @submit.prevent="submitReservation">
-
-        <label for="checkin">Check-in Date: *</label>
-        <input v-model="reservation.checkin" type="date" id="checkin" required>
-
-        <label for="checkout">Check-out Date: *</label>
-        <input v-model="reservation.checkout" type="date" id="checkout" required>
-
-        <label for="guests">Adults</label>
-        <select v-model="reservation.guests" id="guests" required>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-        </select>
-        <button type="submit">see options</button>
-    </form>
-    `
 }).mount('#booking-form')
